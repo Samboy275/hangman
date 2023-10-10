b1e597ce5c7fe51b873ac655947d114405ec3412
samual moahmed <sam-wal@windowslive.com>
2023-10-10 17:04:41 +0300

    Implement basic user guess input handling fix bugs with loading and saving

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -2,18 +2,24 @@ require './get_words'
 require 'yaml'
 
 class Hangman
-  attr_reader :tries, :player_name, :word
+  attr_reader :tries, :player_name, :word, :game_ended
 
-  def initialize(player_name = '', tries = 10, word = '')
-    @player_name = player_name
-    @word = word
-    @tries = tries
-  end
-
-  def self. initialize_from_load(ser_obj)
-    @word = ser_obj[:word]
-    @tries = ser_obj[:tries]
-    @player_name = ser_obj[:player_name]
+  def initialize(kwargs)
+    if kwargs[:player_name].strip == ''
+      puts 'Please enter a valid name'
+      return
+    end
+    @player_name = kwargs[:player_name]
+    @game_ended = false
+    if kwargs[:saved_load] == true
+      @word = kwargs[:word]
+      @tries = kwargs[:tries]
+      @guess = kwargs[:guess]
+    else
+      @word = ''
+      @tries = 10
+      @guess = ''
+    end
   end
 
   def select_word()
@@ -21,26 +27,36 @@ class Hangman
     while @word.length < 5 || @word.length > 12
       @word = File.open('../words.txt', 'r'){|file| file.readlines.sample.strip}
     end
+    @guess = '_' * @word.length
+  end
 
+  def player_guess(guess)
+    if @word.include?(guess)
+      puts 'u guessed right'
+      @game_ended = true
+    else
+      @tries = @tries - 1
+      puts "you gussed wrong #{@tries} left"
+    end
   end
 
   def to_s()
     "#{@player_name} #{@tries} #{@word}"
   end
 
+  # serializtion methods for saving and loading
   def to_yaml
     serilized_object = YAML.dump({
       :player_name => @player_name,
       :word => @word,
       :tries => @tries,
+      :saved_load => true
     })
     serilized_object
   end
 
-  def self.from_ymal(serilized_object)
-    hangman = Hangman.new(player_name=serilized_object[:player_name],
-                          tries=serilized_object[:tries],
-                          word=serilized_object[:word])
+  def self.from_yaml(serilized_object)
+    hangman = Hangman.new(serilized_object)
     return hangman
   end
 
@@ -54,16 +70,18 @@ class Hangman
 
   def self.load_game(player_name)
     ser_obj = YAML.load(File.read("../saved\ games/#{player_name}.yml"))
-    return self.from_ymal(ser_obj)
+    return self.from_yaml(ser_obj)
   end
 
 end
 
 
-hangman = Hangman.new('samual')
+hangman = Hangman.new(player_name:'samual')
 hangman.select_word
 puts hangman
 
-hangman.save_game()
-
-puts Hangman.load_game('samual')
+while hangman.tries > 0
+  guess = gets.chomp
+  hangman.player_guess(guess)
+  break if hangman.game_ended
+end
